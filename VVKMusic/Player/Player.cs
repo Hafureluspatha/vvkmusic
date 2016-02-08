dab0a594d40f6bc0da7d3d9efd4dc7af570613fd
Hafureluspatha <xxxddr3@gmail.com>
2016-02-08 22:22:25 +0800

    Attempt of equalizer implementation

--- VVKMusic/Player/Player.cs
+++ VVKMusic/Player/Player.cs
@@ -2,6 +2,7 @@
 using System.Windows.Media;
 using Un4seen.Bass;
 using Un4seen.Bass.AddOn.Wma;
+using Un4seen.Bass.AddOn.Fx;
 using Un4seen.Bass.Misc;
 using Common;
 using Status = Common.Common.Status;
@@ -10,27 +11,54 @@ namespace Player
 {
     public class Player : IPlayer
     {
-        private int stream;
-
+        private int _stream;
+        private int _fxEQ;
         public Player()
         {
             BassNet.Registration("xxxddr3@gmail.com", "2X441017152222");
             if(!(Bass.BASS_Init(-1, 44100, BASSInit.BASS_DEVICE_DEFAULT, IntPtr.Zero)))
             {
-                throw new Exception();
+                throw new Exception("Unable to initialize instance of Player class");
             }
+            SetBFX_EQ(_stream);
+        }
+        private void SetBFX_EQ(int channel)
+        {
+            _fxEQ = Bass.BASS_ChannelSetFX(channel, BASSFXType.BASS_FX_BFX_PEAKEQ, 0);
+            BASS_BFX_PEAKEQ eq = new BASS_BFX_PEAKEQ();
+            eq.fQ = 2f;
+            eq.fBandwidth = 2.5f;
+            eq.lChannel = BASSFXChan.BASS_BFX_CHANALL;
+
+            float[] InsertedValues = new float[18]{31f,63f,87f,125f,175f,250f,350f,500f,700f,
+                1000f,1400f,2000f,2800f,4000f,5600f,8000f,11200f,16000f};
+            for (int i = 0; i < 18; ++i)
+            {
+                eq.lBand = i;
+                eq.fCenter = InsertedValues[i];
+                Bass.BASS_FXSetParameters(_fxEQ, eq);
+                UpdateFX(i, 0f);
+            }
+        }
+        private void UpdateFX(int band, float gain)
+        {
+            BASS_BFX_PEAKEQ eq = new BASS_BFX_PEAKEQ();
+            eq.lBand = band;
+            Bass.BASS_FXGetParameters(_fxEQ, eq);
+            eq.fGain = gain;
+            Bass.BASS_FXSetParameters(_fxEQ, eq);
         }
         public Status SetSource(Song playedSong)
         {
             if (playedSong.Downloaded)
             {
-                stream = Bass.BASS_StreamCreateFile(playedSong.DownloadedUri.LocalPath, 0, 0, BASSFlag.BASS_DEFAULT);
+                _stream = Bass.BASS_StreamCreateFile(playedSong.DownloadedUri.LocalPath, 0, 0, BASSFlag.BASS_DEFAULT);
             }
             else
             {
-                stream = Bass.BASS_StreamCreateURL(playedSong.Uri.ToString(), 0, 0, null, new IntPtr(0));
+                _stream = Bass.BASS_StreamCreateURL(playedSong.Uri.ToString(), 0, 0, null, new IntPtr(0));
             }
-            if (stream != 0)
+            if (_stream != 0)
             {
                 return Status.OK;
             }
@@ -39,9 +67,9 @@ namespace Player
                 if (playedSong.Downloaded)
                 {
                     playedSong.Downloaded = false;
-                    stream = Bass.BASS_StreamCreateURL(playedSong.Uri.ToString(), 0, 0, null, new IntPtr(0));
+                    _stream = Bass.BASS_StreamCreateURL(playedSong.Uri.ToString(), 0, 0, null, new IntPtr(0));
                 }
-                if (stream != 0)
+                if (_stream != 0)
                 {
                     return Status.OK;
                 }
@@ -53,7 +81,7 @@ namespace Player
         }
         public Status Play()
         {
-            if(Bass.BASS_ChannelPlay(stream, false))
+            if(Bass.BASS_ChannelPlay(_stream, false))
             {
                 return Status.OK;
             }
@@ -64,7 +92,7 @@ namespace Player
         }
         public Status Stop()
         {
-            if (Bass.BASS_ChannelStop(stream))
+            if (Bass.BASS_ChannelStop(_stream))
             {
                 return Status.OK;
             }
@@ -75,7 +103,7 @@ namespace Player
         }
         public Status Pause()
         {
-            if (Bass.BASS_ChannelPause(stream))
+            if (Bass.BASS_ChannelPause(_stream))
             {
                 return Status.OK;
             }
@@ -90,7 +118,7 @@ namespace Player
         }
         ~Player()
         {
-            Bass.BASS_StreamFree(stream);
+            Bass.BASS_StreamFree(_stream);
             Bass.BASS_Free();
         }
     }
